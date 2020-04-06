a9712b87b601067a7d22e8e92c96f0d3ff4a0994
JosephCharuhas <joseph.charuhas22@bishopireton.org>
2020-04-06 17:56:35 -0400

    Ruby kermit

---
+++ src/main/java/drewJoseph/apcsamod/ItemBase.java
@@ -0,0 +1,26 @@
+package drewJoseph.apcsamod;
+
+import drewJoseph.apcsamod.init.ModItems;
+import drewJoseph.apcsamod.util.IHasModel;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item implements IHasModel
+{
+		public ItemBase(String name)
+		{
+			setUnlocalizedName(name);
+			setRegistryName(name);
+			setCreativeTab(CreativeTabs.MATERIALS);
+			
+			ModItems.ITEMS.add(this);
+		}
+
+	@Override
+	public void registerModels() 
+	{
+		// TODO Auto-generated method stub
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+
+}
--- src/main/java/drewJoseph/apcsamod/init/ModItems.java
+++ src/main/java/drewJoseph/apcsamod/init/ModItems.java
@@ -3,6 +3,7 @@ package drewJoseph.apcsamod.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import drewJoseph.apcsamod.ItemBase;
 import drewJoseph.apcsamod.Wand;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
@@ -11,6 +12,7 @@ import net.minecraft.item.ItemSword;
 public class ModItems {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	public static final ItemSword Wand = new Wand(ToolMaterial.IRON, "wand");	
+	public static final Item RUBY = new ItemBase("ruby");
 	
 	
 }
